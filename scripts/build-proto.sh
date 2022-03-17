da001f09690b75d3d81de6ac7a06054bb0dadf5c
Luke Todd <luketodd@zoho.com>
2022-03-17 10:40:48 +0000

    Updated proto build

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# $1 is the serrvice name
+# $2 is the service proto file name
+
 protoc --proto_path=. --go_out=. --micro_out=. \
-		$1/proto/consignment/consignment.proto
+		$1/proto/$2/$2.proto
